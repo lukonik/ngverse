8ea5746621bab94fc3f441b54091801633053ec8
luka onik <luka.onikadze@toptal.com>
2025-08-26 15:38:42 +0400

    docs: remove nesting from ui-links

--- projects/docs/src/app/features/sidebar/ui-links.ts
+++ projects/docs/src/app/features/sidebar/ui-links.ts
@@ -14,163 +14,123 @@ export const UI_LINKS: SidebarGroup = {
   ],
   children: [
     {
-      name: 'Form',
-      children: [
-        {
-          name: 'Input',
-          url: 'input',
-        },
-        {
-          name: 'Form Field',
-          url: 'form-field',
-        },
-        {
-          name: 'Multi Select',
-          url: 'multi-select',
-        },
-        {
-          name: 'OTP Input',
-          url: 'otp-input',
-        },
-        {
-          name: 'Select',
-          url: 'select',
-        },
-        {
-          name: 'Checkbox',
-          url: 'checkbox',
-        },
-        {
-          name: 'Radio Button',
-          url: 'radio-button',
-        },
-        {
-          name: 'Textarea',
-          url: 'textarea',
-        },
-        {
-          name: 'Switch',
-          url: 'switch',
-        },
-      ],
+      name: 'Input',
+      url: 'input',
     },
     {
-      name: 'Panel',
-      children: [
-        {
-          name: 'Accordion',
-          url: 'accordion',
-        },
-        {
-          name: 'Alert',
-          url: 'alert',
-        },
-        {
-          name: 'Card',
-          url: 'card',
-        },
-        {
-          name: 'Tab',
-          url: 'tab',
-        },
-      ],
+      name: 'Form Field',
+      url: 'form-field',
+    },
+    {
+      name: 'Multi Select',
+      url: 'multi-select',
+    },
+    {
+      name: 'OTP Input',
+      url: 'otp-input',
+    },
+    {
+      name: 'Select',
+      url: 'select',
+    },
+    {
+      name: 'Checkbox',
+      url: 'checkbox',
+    },
+    {
+      name: 'Radio Button',
+      url: 'radio-button',
+    },
+    {
+      name: 'Textarea',
+      url: 'textarea',
+    },
+    {
+      name: 'Switch',
+      url: 'switch',
+    },
+    {
+      name: 'Accordion',
+      url: 'accordion',
+    },
+    {
+      name: 'Alert',
+      url: 'alert',
+    },
+    {
+      name: 'Card',
+      url: 'card',
+    },
+    {
+      name: 'Tab',
+      url: 'tab',
     },
 
     {
       name: 'Button',
-      children: [
-        {
-          name: 'Button',
-          url: 'button',
-        },
-      ],
+      url: 'button',
     },
     {
-      name: 'Overlay',
-      children: [
-        {
-          name: 'Dialog',
-          url: 'dialog',
-        },
-        {
-          name: 'Drawer',
-          url: 'drawer',
-        },
-        {
-          name: 'Popover',
-          url: 'popover',
-        },
-        {
-          name: 'Toast',
-          url: 'toast',
-        },
-        {
-          name: 'Tooltip',
-          url: 'tooltip',
-        },
-      ],
+      name: 'Dialog',
+      url: 'dialog',
     },
     {
-      name: 'Menu',
-      children: [
-        {
-          name: 'Context Menu',
-          url: 'context-menu',
-        },
-      ],
+      name: 'Drawer',
+      url: 'drawer',
     },
     {
-      name: 'Data',
-      children: [
-        {
-          name: 'Pagination',
-          url: 'pagination',
-        },
-      ],
+      name: 'Popover',
+      url: 'popover',
     },
     {
-      name: 'Misc',
-      children: [
-        {
-          name: 'Dark Mode',
-          url: 'dark-mode',
-        },
-        {
-          name: 'Divider',
-          url: 'divider',
-        },
-        {
-          name: 'Loader',
-          url: 'loader',
-        },
+      name: 'Toast',
+      url: 'toast',
+    },
+    {
+      name: 'Tooltip',
+      url: 'tooltip',
+    },
+    {
+      name: 'Context Menu',
+      url: 'context-menu',
+    },
+    {
+      name: 'Pagination',
+      url: 'pagination',
+    },
+    {
+      name: 'Dark Mode',
+      url: 'dark-mode',
+    },
+    {
+      name: 'Divider',
+      url: 'divider',
+    },
+    {
+      name: 'Loader',
+      url: 'loader',
+    },
 
-        {
-          name: 'Progress Bar',
-          url: 'progress-bar',
-        },
-        {
-          name: 'Skeleton',
-          url: 'skeleton',
-        },
-        {
-          name: 'Badge',
-          url: 'badge',
-        },
-      ],
+    {
+      name: 'Progress Bar',
+      url: 'progress-bar',
     },
+    {
+      name: 'Skeleton',
+      url: 'skeleton',
+    },
+    {
+      name: 'Badge',
+      url: 'badge',
+    },
+    {
+      name: 'Font Icon',
+      url: 'font-icon',
+    },
+
     {
       name: 'Icon',
-      children: [
-        {
-          name: 'Font Icon',
-          url: 'font-icon',
-        },
-
-        {
-          name: 'Icon',
-          url: 'icon',
-        },
-      ],
+      url: 'icon',
     },
-  ],
+  ].sort((a, b) => a.name.localeCompare(b.name)),
 };
